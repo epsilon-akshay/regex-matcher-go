d676340d4c9d04917b11b381c88afd701a6a06f7
akshay.nambiar <akshay.nambiar@go-jek.com>
2019-06-11 21:46:13 +0530

    Updated with the dp implemnetation of regex

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-<<<<<<< HEAD
 import (
 	"bufio"
 	"fmt"
@@ -13,9 +12,30 @@ func main() {
 	word, _ := reader.ReadString('\n')
 	fmt.Println("enter the regex pattern you wish to input")
 	regex, _ := reader.ReadString('\n')
-	fmt.Printf("the word and the regex is %v %v", word, regex)import "fmt"
+	fmt.Printf("the word and the regex is %v %v", word, regex)
+	var stack string
+	compareString := getSetOfString(regex, stack)
+	for i := 0; i < len(compareString); i++ {
+		if regex[i] != compareString[i] {
+			fmt.Println("false")
+			return
+		}
+	}
+	fmt.Println("true")
+}
 
-func main() {
-	fmt.Println("vim-go")
-	fmt.Println("asdasd")
+func getSetOfString(regex string, stack string) string {
+	fmt.Println(len(regex), string(regex[3]))
+	for i := 1; i < len(regex)-1; i = i + 1 {
+		if regex[i-1] == '[' {
+			for j := i; j < len(regex); j++ {
+				stack = stack + string(regex[j])
+				if regex[j] == ']' {
+					i = j
+					break
+				}
+			}
+		}
+	}
+	return stack
 }
